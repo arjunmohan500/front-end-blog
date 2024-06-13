454e96d5192e9e13dd3e83f1851d8c5f81e808dc
arjunmohan500 <arjunmohan500@gmail.com>
2024-06-13 14:24:27 +0530

    created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Signup from './Signup';
+import Signin from './components/Signin';
 
 function App() {
   return (
     <div>
       <Signup/>
+      <Signin/>
     </div>
   );
 }
